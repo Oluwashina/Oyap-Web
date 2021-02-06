1d3bef4e4900a8eb02d8733713b5d88ab892a7bb
Emrick19 <emricky17@gmail.com>
2021-02-06 17:13:07 +0100

    Add authReducer

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,31 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initState = {
+  authError: null,
+};
+
+const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    case actionTypes.SIGNUP_START:
+        console.log("signup start");
+      return {
+        ...state,
+        error: null,
+      };
+    case actionTypes.SIGNUP_SUCCESS:
+      console.log("signup success");
+      return {
+        ...state,
+        error: null,
+      };
+    case actionTypes.SIGNUP_FAIL:
+      console.log("signup failed");
+      return {
+        authError: action.err.message,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
