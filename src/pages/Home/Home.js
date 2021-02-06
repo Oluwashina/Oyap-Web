7f7f2e07005a23bc556eadcb50c6958fdf642ffd
Emrick19 <emricky17@gmail.com>
2021-02-06 23:28:43 +0100

    Update Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { connect, useSelector } from 'react-redux'
 import * as actions from '../../store/actions'
 
 const Home = ({logout}) => {
+
+    const role = useSelector(state => state.firebase.profile.role)
+    
     return (
         <div>
-            <div>Home</div>
+            <div>Hello, {role}</div>
             <button type="sumbit" className="btn btn-primary" onClick={logout}> Log out</button>
         </div>
     )
