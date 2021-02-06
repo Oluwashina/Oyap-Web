8c4cfe29ef1564900773957afe90f52d79bff9d1
Emrick19 <emricky17@gmail.com>
2021-02-06 17:10:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import UserLogin from './pages/Login/userLogin'
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import UserSignup from "./pages/Signup/UserSignup";
+import UserLogin from "./pages/Login/UserLogin";
+import Home from "./pages/Home/Home"
+
+import { useSelector } from 'react-redux'
+
 
 function App() {
+  const auth = useSelector((state) => state.firebase.auth);
+  console.log(auth);
   return (
-    <Router>
-      <Switch>
-          <Route exact path="/" component={UserLogin} />
-      </Switch>
-    </Router>
+    <>
+      <div className="container d-flex align-items-center justify-content-center min-vh-100">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={UserSignup} />
+            <Route path="/login" component={UserLogin} />
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
