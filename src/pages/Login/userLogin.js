cef6b30f96642bce596e39891e84874a9b801441
Emrick19 <emricky17@gmail.com>
2021-02-06 17:11:31 +0100

    Update UserLogin.js

--- src/pages/Login/userLogin.js
+++ src/pages/Login/userLogin.js
@@ -1,12 +1,74 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
+import * as actions from "../../store/actions";
 
+const UserSignup = (props) => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    role: "Farmer",
+  });
 
-const UserLogin = () =>{
-    return(
-        <div>
-            <p>Helo</p>
-        </div>
-    )
-}
+  const history = useHistory();
 
-export default UserLogin;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.login(user);
+
+    history.push("/");
+  };
+  return (
+    <div className="card w-50">
+      <h2 className="text-center mt-4">Log In</h2>
+      <div className="card-body">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="form-control"
+              required
+              onChange={handleChange}
+              value={user.email}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="form-control"
+              required
+              onChange={handleChange}
+              value={user.password}
+            />
+          </div>          
+          <button type="submit" className="btn btn-primary btn-block">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: (credentials) => dispatch(actions.signIn(credentials)),
+  };
+};
+export default connect(null, mapDispatchToProps)(UserSignup);
