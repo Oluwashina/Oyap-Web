a1065e158da22ca22f9a12768b4295f472798c79
Emrick19 <emricky17@gmail.com>
2021-02-06 17:13:20 +0100

    Update rootReducer.js

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,7 +2,10 @@ import { combineReducers } from "redux";
 import { firestoreReducer } from "redux-firestore";
 import { firebaseReducer } from "react-redux-firebase";
 
+import authReducer from './auth'
+
 const rootReducer = combineReducers({
+  auth: authReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer,
 });
