cb40408b25a814c8b2704c9b82cb5cd91cefab81
Emrick19 <emricky17@gmail.com>
2021-02-17 23:31:24 +0100

    Update userLogin.js

--- src/pages/Login/userLogin.js
+++ src/pages/Login/userLogin.js
@@ -1,32 +1,38 @@
 import React from "react";
 import Logo from "../../assets/images/logo.png";
 import WelcomeImg from "../../assets/images/welcome-img.png";
+import "./Login.css";
 
 const UserLogin = () => {
   return (
     <div className="conatiner-fluid">
       <div class="row">
-        <div class="col-sm">
-          <img src={Logo} alt="oyap logo" />
-          <div>
-            <p>Login As</p>
+        <div class="col-4">
+          <img className="logo" src={Logo} alt="oyap logo" />
+          <div className="container">
+            <p className="form-header">Login As</p>
             <form>
               <div className="form-group">
                 <label htmlFor="Email">Enter your email address</label>
-                <input className="form-control" placeholder="sample@example.com" />
+                <input
+                  className="form-control"
+                  placeholder="sample@example.com"
+                />
               </div>
               <div className="form-group">
                 <label htmlFor="Password">Enter your password</label>
                 <input className="form-control" placeholder="password" />
               </div>
-              <button className="btn btn-primary btn-block">Login</button>
+              <button className="btn btn-primary btn-block text-uppercase">Login</button>
             </form>
             <p>Forget password?</p>
-            <hr/>
-            <button className="btn btn-primary btn-block">Sign Up</button>
+            <hr />
+            <button className="btn btn-outline-primary btn-block text-uppercase">
+              Sign Up
+            </button>
           </div>
         </div>
-        <div class="col-sm">
+        <div class="col-8">
           <img src={WelcomeImg} alt="oyap" />
         </div>
       </div>
