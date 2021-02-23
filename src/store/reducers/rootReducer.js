61f8adbdfb91c899fbb1ab26be0455c8fc79a02f
Emrick19 <emricky17@gmail.com>
2021-02-23 19:59:45 +0100

    Import rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,9 +3,11 @@ import { firestoreReducer } from "redux-firestore";
 import { firebaseReducer } from "react-redux-firebase";
 
 import authReducer from './auth'
+import productsReducer from './products'
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  products: productsReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer,
 });
