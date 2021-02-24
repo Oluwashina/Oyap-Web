a0b318d2e746da63803eb91b936752441f1903a0
Emrick19 <emricky17@gmail.com>
2021-02-24 22:02:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UserSignup from "./pages/Signup/UserSignup";
@@ -6,8 +7,8 @@ import UserLogin from "./pages/Login/userLogin";
 import ForgotPassword from "./pages/ForgotPassword/forgotPassword";
 import ResetPassword from "./pages/ResetPassword/resetPassword";
 import CreateProduct from "./components/CreateProduct/CreateProduct";
-// import Home from "./pages/Home/Home"
-// import PrivateRoute from "./components/PrivateRoute"
+import Home from "./pages/Home/Home"
+import PrivateRoute from "./components/PrivateRoute"
 
 import { useSelector } from "react-redux";
 import BuyerHome from "./pages/Home/BuyerHome";
@@ -16,15 +17,23 @@ import CheckoutPage from "./pages/Home/BuyerCheckout"
 import Cart from "./pages/Home/BuyerCart"
 
 function App() {
-  const state = useSelector((state) => state.firebase);
-  console.log(state);
+  const role = useSelector((state) => state.firebase.profile.role);
+  let homeRoute = null
+  if(role === 'Buyer'){
+    homeRoute = (
+      <PrivateRoute exact path="/" component={BuyerHome} />
+    )
+  }else{
+    homeRoute = (
+      <PrivateRoute exact path="/" component={Home} />
+      )
+  }
   return (
     <>
       <div className="">
         <Router>
-          <Switch>
-            {/* <PrivateRoute exact path="/" component={Home} /> */}
-            <Route exact path="/" component={BuyerHome} />
+          <Switch>        
+            {homeRoute}
             <Route path="/item/:id" component={BuyerItemPage} />
             <Route path="/checkout" component={CheckoutPage} />
             <Route path="/cart" component={Cart} />
