8a23d3e0b4abff4f6ac2746146fd308099e08b8d
Emrick19 <emricky17@gmail.com>
2021-03-03 22:54:38 +0100

    Add checkoutValidator

--- src/validationSchema/validator.js
+++ src/validationSchema/validator.js
@@ -1,36 +1,58 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const loginValidator = Yup.object({
-    email: Yup.string().email("Enter a valid email")
+  email: Yup.string()
+    .email("Enter a valid email")
     .required("Email is required"),
-    password: Yup.string()
+  password: Yup.string()
     .min(3, "Password cannot be less than 3 characters")
     .required("Password is required"),
 });
 
 export const forgotPasswordValidator = Yup.object({
-    email: Yup.string().email("Enter a valid email")
+  email: Yup.string()
+    .email("Enter a valid email")
     .required("Email is required"),
 });
 
 export const resetPasswordValidator = Yup.object({
-    password: Yup.string()
+  password: Yup.string()
     .min(6, "Password cannot be less than 6 characters")
     .required("Password is required"),
-    confirm_password:Yup.string().required('Passwords must match').oneOf([Yup.ref('password'), null], 'Passwords must match')
+  confirm_password: Yup.string()
+    .required("Passwords must match")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
 });
 
 export const registerValidator = Yup.object({
-    firstname: Yup.string().required("Firstname is required"),
-    lastname: Yup.string().required("Lastname is required"),
-    phone: Yup.string()
-    .required("Phonenumber is required").matches(
-      /(^[+]?[234]\d{12}$)/,
-      "Enter a valid phone number"),
-    email: Yup.string().email("Enter a valid email")
+  firstname: Yup.string().required("Firstname is required"),
+  lastname: Yup.string().required("Lastname is required"),
+  phone: Yup.string()
+    .required("Phonenumber is required")
+    .matches(/(^[+]?[234]\d{12}$)/, "Enter a valid phone number"),
+  email: Yup.string()
+    .email("Enter a valid email")
     .required("Email is required"),
-    password: Yup.string()
+  password: Yup.string()
     .min(6, "Password cannot be less than 6 characters")
     .required("Password is required"),
-    confirm_password:Yup.string().required('Passwords must match').oneOf([Yup.ref('password'), null], 'Passwords must match')
+  confirm_password: Yup.string()
+    .required("Passwords must match")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
+
+export const checkoutValidator = Yup.object({
+  firstName: Yup.string().required("Firstname is required"),
+  lastName: Yup.string().required("Lastname is required"),
+
+  state: Yup.string()
+    .required("State is required"),
+  city: Yup.string().required("City is required"),
+  street: Yup.string().required("Firstname is required"),
+  phone1: Yup.string()
+    .required("Phonenumber is required")
+    .matches(/(^[+]?[234]\d{12}$)/, "Enter a valid phone number"),
+  phone2: Yup.string()
+    .required("Additional phone number is required")
+    .matches(/(^[+]?[234]\d{12}$)/, "Enter a valid phone number"),
 });
