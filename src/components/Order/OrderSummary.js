16080f7ea44f2ee27c0d9e3b6136551bcff8d151
Emrick19 <emricky17@gmail.com>
2021-03-07 20:11:04 +0100

    Modify OrderSummary.js

--- src/components/Order/OrderSummary.js
+++ src/components/Order/OrderSummary.js
@@ -1,78 +1,77 @@
-import React,{useState, useEffect} from "react";
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom' 
-import Order from "../../components/Order/Order"
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Order from "../../components/Order/Order";
 
 const OrderSummary = (props) => {
+  const { cartItems, handleOrder, orderData } = props;
 
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [shippingFee] = useState(2000);
 
-  const {cartItems, handleOrder} = props
+  useEffect(() => {
+    let price = 0;
 
-  
-  const [totalPrice, setTotalPrice] = useState(0)
-  const [shippingFee] = useState(2000)
+    cartItems.forEach((item) => {
+      price += item.cartQty * item.price;
+    });
 
-  useEffect(() =>{
-      let price = 0;
+    setTotalPrice(price + shippingFee);
+  }, [cartItems, totalPrice, setTotalPrice, shippingFee]);
 
-      cartItems.forEach(item =>{
-          price += item.cartQty * item.price
-      })
-
-      setTotalPrice(price + shippingFee)
-  }, [cartItems, totalPrice, setTotalPrice, shippingFee])
-
-
-  
   const itemsCart = cartItems.length ? (
-    cartItems.map(items=>{
-    return (
+    cartItems.map((items) => {
+      return (
         <div key={items.id}>
-            <div
-              className="mt-4"
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <div>
-                <p
-                  className="mb-0"
-                  style={{ fontSize: 14, width: "70%", lineHeight: "21px" }}
-                >
-                  {items.name}
-                </p>
-              </div>
-              <div>
-                <p className="mb-0" style={{ fontWeight: 600 }}>
-                  NGN {items.subTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                </p>
-              </div>
-            </div>
-
+          <div
+            className="mt-4"
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
             <div>
-              <hr
-                className="mt-4 mb-0"
-                style={{ borderTop: "1px solid rgba(196, 196, 196, 0.5)" }}
-              />
+              <p
+                className="mb-0"
+                style={{ fontSize: 14, width: "70%", lineHeight: "21px" }}
+              >
+                {items.name}
+              </p>
             </div>
-        </div>   
-        )
-        })
-        ) : (
-            
-        <div className="center">
-            <div className="text-center mt-4">
-                <h5 className="mb-0">Your cart is empty!</h5>
-                <p className="mb-0 mt-3">Browse our items and discover our best deals</p>
+            <div>
+              <p className="mb-0" style={{ fontWeight: 600 }}>
+                NGN{" "}
+                {items.subTotal
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </p>
             </div>
-            <div className="text-center">
-                <Link to="/" className="btn btn-sell mt-4">Start Shipping</Link>
-               </div>
-         </div>                       
-        )
+          </div>
 
+          <div>
+            <hr
+              className="mt-4 mb-0"
+              style={{ borderTop: "1px solid rgba(196, 196, 196, 0.5)" }}
+            />
+          </div>
+        </div>
+      );
+    })
+  ) : (
+    <div className="center">
+      <div className="text-center mt-4">
+        <h5 className="mb-0">Your cart is empty!</h5>
+        <p className="mb-0 mt-3">
+          Browse our items and discover our best deals
+        </p>
+      </div>
+      <div className="text-center">
+        <Link to="/" className="btn btn-sell mt-4">
+          Start Shipping
+        </Link>
+      </div>
+    </div>
+  );
 
   return (
     <>
-      
       <div
         className="mt-lg-5 mt-4"
         style={{
@@ -101,14 +100,15 @@ const OrderSummary = (props) => {
           />
         </div>
 
-          {itemsCart}
+        {itemsCart}
 
-{/* shipping amount section */}
+        {/* shipping amount section */}
         <div
           className="mt-4"
-          style={{ display: cartItems.length ? 'flex' : 'none',
-          justifyContent: cartItems.length ? "space-between" : "normal"
-        }} 
+          style={{
+            display: cartItems.length ? "flex" : "none",
+            justifyContent: cartItems.length ? "space-between" : "normal",
+          }}
         >
           <div>
             <p
@@ -128,18 +128,21 @@ const OrderSummary = (props) => {
         <div>
           <hr
             className="mt-4 mb-0"
-            style={{ borderTop: cartItems.length ? '1px solid rgba(196, 196, 196, 0.5)' : 'none',
-          }}
-          
+            style={{
+              borderTop: cartItems.length
+                ? "1px solid rgba(196, 196, 196, 0.5)"
+                : "none",
+            }}
           />
         </div>
 
         <div
-        className="mt-4"
-          style={{ display: cartItems.length ? 'flex' : 'none',
-          justifyContent: cartItems.length ? "space-between" : "normal"
-        } } 
-        > 
+          className="mt-4"
+          style={{
+            display: cartItems.length ? "flex" : "none",
+            justifyContent: cartItems.length ? "space-between" : "normal",
+          }}
+        >
           <div>
             <p className="mb-0" style={{ fontWeight: 700 }}>
               TOTAL
@@ -153,24 +156,23 @@ const OrderSummary = (props) => {
         </div>
       </div>
 
-        {/* place order button */}
-      <div className="mt-4">              
-            <Order
-              handleOrder={handleOrder}
-              amount={totalPrice}
-                handleDisabled={cartItems}
-                 />
-        </div>
-      
-
+      {/* place order button */}
+      <div className="mt-4">
+        <Order
+          handleOrder={handleOrder}
+          amount={totalPrice}
+          handleDisabled={cartItems}
+          customer={orderData}
+        />
+      </div>
     </>
   );
 };
 
-const mapStateToProps = (state) =>{
-  return{
-      cartItems: state.cart.cartItems,
-  }
-}
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state.cart.cartItems,
+  };
+};
 
 export default connect(mapStateToProps)(OrderSummary);
