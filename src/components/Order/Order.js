74be8c64bf3e147c57c965c3ed2707531a7eea2f
Emrick19 <emricky17@gmail.com>
2021-03-13 20:54:47 +0100

    Update Order.js

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
 
-const Payment = ({handleOrder, handleDisabled, amount, customerDetails, errors}) => {
+const Payment = ({
+  handleOrder,
+  handleDisabled,
+  amount,
+  customerDetails,
+  errors,
+}) => {
   const config = {
     public_key: process.env.REACT_APP_FLUTTERWAVE_PUBLIC_KEY,
     tx_ref: Date.now(),
@@ -21,18 +27,36 @@ const Payment = ({handleOrder, handleDisabled, amount, customerDetails, errors})
     },
   };
 
+  const [error, setError] = useState(null);
+
+  // function isNotEmpty(obj) {
+  //   return Object.keys(obj).length === 0;
+  // }
+
+  useEffect(() => {
+    if (Object.keys(errors).length > 0) {
+      setError(errors);
+    }
+  }, [errors]);
+
+  // const isEmpty = Object.values(customerDetails).every(
+  //   (customerInfo) => customerInfo === null || customerInfo === ""
+  // );
+
   const handleFlutterPayment = useFlutterwave(config);
 
   return (
     <>
       <button
         className="btn btn-place btn-block mt-4"
-        disabled={handleDisabled.length === 0}
-        onClick={() => {          
+        disabled={
+          handleDisabled.length === 0 || error ? true : false
+        }
+        onClick={() => {
           handleFlutterPayment({
             callback: (response) => {
               console.log(response);
-              handleOrder(customerDetails)
+              handleOrder(customerDetails);
               closePaymentModal(); // this will close the modal programmatically
             },
             onClose: () => {},
