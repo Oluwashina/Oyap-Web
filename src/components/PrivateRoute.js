0153d9a3173a0c2f6a21fa2b2b7050041a92e6cf
Emrick19 <emricky17@gmail.com>
2021-03-16 18:37:01 +0100

    Update PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,25 +1,16 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { isLoaded, isEmpty } from "react-redux-firebase";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const {
-    auth,
-    profile: { role },
-  } = useSelector((state) => state.firebase.auth);
+  const auth = useSelector((state) => state.firebase.auth);
+
 
   return (
     <Route
       {...rest}
       render={(props) =>
-        !auth.uid ? (
-          <Redirect
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        ) : (
-          <Component {...props} />
-        )
+        !auth.uid ? <Redirect to="/login" /> : <Component {...props} />
       }
     />
   );
