0d8109f2398b45c8cdc2fcc0458631d34a849bdb
Emrick19 <emricky17@gmail.com>
2021-04-06 09:49:46 +0100

    Implement Logistic Private Route

---
+++ src/components/PrivateRoutes/LogisticRoute.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+
+const BuyerRoute = ({ component: Component, isAuth, role, ...rest }) => {
+  console.log(isAuth);
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!isAuth) {
+          return (
+            <Redirect
+              to={{ pathname: "/login", state: { from: props.location } }}
+            />
+          );
+        }
+        if (role !== "Logistic") {
+          // role not authorised so redirect to unnauthorised page
+          alert("You do not have access to this page as !" + role);
+          return <Redirect to="/faq" />;
+        }
+        return <Component {...props} />;
+      }}
+    />
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.auth.isAuthenticated,
+    role: state.auth.role,
+  };
+};
+export default connect(mapStateToProps)(BuyerRoute);
